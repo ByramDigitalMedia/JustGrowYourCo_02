2c197c97a9837c78f0f30f5bc36f7abc2769fd98
ByramDigitalMedia <contact@byramdigitalmedia.com>
2025-08-06 17:53:35 -0400

    first commit - second iteration - Hi Maureen!

---
+++ README.md
@@ -0,0 +1,12 @@
+## Just Grow Your Co
+
+Just Grow Your Co. - Maureen Forrester - Mulitpage website. The site is fully responsive and designed with HTML, CSS and Bootstrap.
+
+## Demo
+
+- Live
+  - [JGYC](https://byramdigitalmedia.github.io/JustGrowYourCo/)
+
+## License
+
+This project is licensed under MIT.
---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* =======================================================
+  * Template Name: Just Grow Your Co 2
+  * Template URL: https://byramdigitalmedia.com
+  * Updated: Aug 6 2025 with Bootstrap
+  * Author: BDM
+  * License: MIT
+  ======================================================== */
+
+let date = new Date().getFullYear()
+document.getElementById("year").innerHTML = date
